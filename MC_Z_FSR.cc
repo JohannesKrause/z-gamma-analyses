2f989b5d1d942181d345be7e5998170bd2ea1f6a
Johannes Krause <johannes.krause1@tu-dresden.de>
2017-04-13 10:51:20 +0200

    Oberservablen geaendert und plot files verbessert. jetzige Observablen: Et:all, kleine dr, große dr;  dr, dhpi, deta, mzgamma, mlgamma, qt

--- MC_Z_FSR.cc
+++ MC_Z_FSR.cc
@@ -8,12 +8,11 @@
 namespace Rivet {
 
    /*Cuts:  
-    * 2 myons, opposite charge, pT> 10 GeV, |eta| < 2.5 
-    * hardest myon: pT> 20 GeV 
-    * photonen: pT>5GeV , |eta| < 1.4
+    * 2 leptons, opposite charge, pT> 15 GeV, |eta| < 2.5 
+    * photonen: pT>5GeV , |eta| < 2.5
     *      -> highest pT-photon selected that does not come from a decay 
-    *         and has a dR in (0.05, 3) to the next myon
-    * myon pair: invariant mass in (30, 87)
+    *         and has a dR in (0.05, 5) to the next myon
+    * lepton pair: invariant mass in (30, 87)
     * 
     *  */
    
@@ -23,33 +22,44 @@ namespace Rivet {
   public:
 
     /// Constructor
-    DEFAULT_RIVET_ANALYSIS_CTOR(MC_Z_FSR);
-
+    MC_Z_FSR(string name="MC_Z_FSR")
+      : Analysis(name)
+    {
+      // mode 1: electron, 2: muon
+      _mode = 2;
+    }
 
     /// @name Analysis methods
     //@{
 
     /// Book histograms and initialise projections before the run
     void init() {
-      Cut c_photons = Cuts::pT >= 5.0*GeV && (Cuts::etaIn(-1.4, 1.4));
+      Cut c_photons = Cuts::pT >= 5.0*GeV && Cuts::abseta < 2.5;
       IdentifiedFinalState photons(c_photons);
       photons.acceptId(PID::PHOTON);
       declare(photons, "PHOTFS");
 
-      Cut c_muons   = Cuts::pT > 10*GeV && Cuts::abseta < 2.4;
-      IdentifiedFinalState muons(c_muons);
-      muons.acceptIdPair(PID::MUON);
-      declare(muons, "MUFS");
+      Cut c_leptons   = Cuts::pT > 15*GeV && Cuts::abseta < 2.5;
+      IdentifiedFinalState leptonfs(c_leptons);
+      if (_mode==1)  {
+		   leptonfs.acceptIdPair(PID::ELECTRON);
+	   }  
+      else leptonfs.acceptIdPair(PID::MUON);
+      declare(leptonfs, "LEPFS");
 
 
-      _hist_pho_et           = bookHisto1D("photon_et_all", 95, 5, 250);  // photon transverse energy
-      _hist_pho_et_wide      = bookHisto1D("photon_et_wide", 95, 5, 250);  // photon transverse energy (0.5 < dr < 3.0)
-      _hist_pho_et_close     = bookHisto1D("photon_et_close", 95, 5, 250);  // photon transverse energy (0.05 < dr < 0.5)
-      _hist_pho_et_lqt       = bookHisto1D("photon_et_lqt", 95, 5, 250);  // photon transverse energy (q_T < 10)
-      _hist_pho_et_hqt       = bookHisto1D("photon_et_hqt", 95, 5, 250);  // photon transverse energy (q_T > 50)
-      _hist_pho_dr           = bookHisto1D("dr_all",60,0,5);  // delta_R
-      _hist_pho_dr_lqt       = bookHisto1D("dr_lqt",60,0,5);  // delta_R (q_T < 10)
-      _hist_pho_dr_hqt       = bookHisto1D("dr_hqt",60,0,5);  // delta_R  (q_T > 50)
+      _hist_pho_et           = bookHisto1D("photon_et_all", 100, 5, 250);  // photon transverse energy
+      _hist_pho_et_wide      = bookHisto1D("photon_et_wide", 100, 5, 250);  // photon transverse energy (0.5 < dr < 3.0)
+      _hist_pho_et_close     = bookHisto1D("photon_et_close", 100, 5, 250);  // photon transverse energy (0.05 < dr < 0.5)
+      _hist_pho_dr           = bookHisto1D("dr_all",80,0.05,5);  // delta_R to the closest lepton
+      _hist_pho_dphi         = bookHisto1D("dphi_all", 80, 0.05, 2); //delta_Phi to the closest lepton
+      _hist_pho_deta         = bookHisto1D("deta_all", 80, 0.05, 3); //delta_Phi to the closest lepton
+      _hist_mZgamma          = bookHisto1D("mZgamma", logspace(100,30, 200) ); //dSigma / dmZgamma
+      _hist_m_gl_next        = bookHisto1D("m_gamma_lepton_next", logspace(80, 10, 250) ); //dSigma / dm(gamma,  closest lepton)
+      _hist_qt               = bookHisto1D("qT", logspace(80, 1, 200) ); //dSigma / dm(gamma,  closest lepton)
+
+
+    
     }
 
 
@@ -57,40 +67,37 @@ namespace Rivet {
     void analyze(const Event& event) {
 
     
-      const Particles muons = apply<IdentifiedFinalState>(event, "MUFS").particlesByPt();
+      const Particles leptonen = apply<IdentifiedFinalState>(event, "LEPFS").particlesByPt();
 
-      if (muons.size() < 2) vetoEvent;
-      if (muons[0].pT()/GeV < 20) vetoEvent;
-      if (muons[0].charge()*muons[1].charge() > 0) vetoEvent;
-      const double mZ = (muons[0].momentum() + muons[1].momentum()).mass();
+      if (leptonen.size() < 2) vetoEvent;
+      if (leptonen[0].charge()*leptonen[1].charge() > 0) vetoEvent;
+      const double mZ = ( leptonen[0].momentum() + leptonen[1].momentum()).mass();
       if (!inRange(mZ, 30*GeV, 87*GeV)) vetoEvent;
 
       const Particles photons = apply<IdentifiedFinalState>(event, "PHOTFS").particlesByPt();
       // We want the photon with the highest pT that does not come from a decay
       foreach(const Particle& p, photons) {
         if (p.fromDecay() || !p.isStable()) continue;
+        const double dR = std::min(deltaR(p,leptonen[0]), deltaR(p,leptonen[1]) );
+        if (!inRange(dR, 0.05, 5.0)) continue;
 
-        const double dR = std::min(deltaR(p, muons[0]), deltaR(p, muons[1]) );
-        if (!inRange(dR, 0.05, 3.0)) continue;
+        const double dphi = std::min(deltaPhi(p,leptonen[0]), deltaPhi(p,leptonen[1]) );
+        const double deta = std::min(deltaEta(p,leptonen[0]), deltaEta(p,leptonen[1]) );
+        const double qT = (leptonen[0].mom() + leptonen[1].mom() + p.mom()).pT();
+        const double mZgamma = (leptonen[0].mom() + leptonen[1].mom() + p.mom()).mass() * GeV; 
 
-        // Calculate the three-body (mu,mu,gamma) transverse momentum
-        const double qT = (muons[0].mom() + muons[1].mom() + p.mom()).pT();
+				const Particle next_lepton = (deltaR(leptonen[0], p) > deltaR(leptonen[1], p) ? leptonen[0] : leptonen[1]);
 
-        // Fill the analysis histograms
-        _hist_pho_et->fill(p.pT()/GeV, event.weight());
-        _hist_pho_dr->fill(dR, event.weight());
+   			 // Fill the analysis histograms
+				_hist_pho_et->fill(p.pT()/GeV, event.weight());
+				_hist_pho_dr->fill(dR, event.weight());
+				_hist_pho_dphi->fill(dphi, event.weight());
+				_hist_pho_deta->fill(deta, event.weight());
+				_hist_mZgamma->fill(mZgamma, event.weight());
+				_hist_m_gl_next->fill((next_lepton.mom() + p.mom()).mass()*GeV, event.weight());
+				_hist_qt->fill(qT*GeV, event.weight());
 
         (dR <= 0.5 ? _hist_pho_et_close : _hist_pho_et_wide)->fill(p.pT()/GeV, event.weight());
-
-        if (qT / GeV < 10.) {
-          _hist_pho_et_lqt->fill(p.pT()/GeV, event.weight());
-          _hist_pho_dr_lqt->fill(dR, event.weight());
-        }
-
-        if (qT / GeV > 50.) {
-          _hist_pho_et_hqt->fill(p.pT()/GeV, event.weight());
-          _hist_pho_dr_hqt->fill(dR, event.weight());
-        }
 
         break; // Exit the loop since we found the highest pT lepton already
       }
@@ -104,35 +111,63 @@ namespace Rivet {
       scale(_hist_pho_et,       crossSection() / sumOfWeights());
       scale(_hist_pho_et_wide,  crossSection() / sumOfWeights());
       scale(_hist_pho_et_close, crossSection() / sumOfWeights());
-      scale(_hist_pho_et_lqt,   crossSection() / sumOfWeights());
-      scale(_hist_pho_et_hqt,   crossSection() / sumOfWeights());
       scale(_hist_pho_dr,       crossSection() / sumOfWeights());
-      scale(_hist_pho_dr_lqt,   crossSection() / sumOfWeights());
-      scale(_hist_pho_dr_hqt,   crossSection() / sumOfWeights());
+      scale(_hist_m_gl_next, crossSection() / sumOfWeights());
+      scale(_hist_mZgamma, crossSection() / sumOfWeights());
+      scale(_hist_pho_deta, crossSection() / sumOfWeights());
+      scale(_hist_pho_dphi, crossSection() / sumOfWeights());
+      
 
     }
 
     //@}
 
+  protected:
+      size_t _mode;
+
 
   private:
-
+   
 
     /// @name Histograms
     //@{
     Histo1DPtr _hist_pho_et;
     Histo1DPtr _hist_pho_et_wide, _hist_pho_et_close;
-    Histo1DPtr _hist_pho_et_lqt,  _hist_pho_et_hqt;
-    Histo1DPtr _hist_pho_dr;
-    Histo1DPtr _hist_pho_dr_lqt, _hist_pho_dr_hqt;
+    Histo1DPtr _hist_pho_dr, _hist_pho_dphi, _hist_pho_deta;
+    Histo1DPtr _hist_mZgamma , _hist_m_gl_next;
+    Histo1DPtr _hist_qt;
     //@}
 
 
   };
 
 
+  class MC_Z_FSR_E : public MC_Z_FSR {
+  public:
+    MC_Z_FSR_E()
+      :MC_Z_FSR("MC_Z_FSR_E")
+    {
+      _mode = 1;
+    }
+  };
+  
+  
+    class MC_Z_FSR_MU : public MC_Z_FSR {
+  public:
+    MC_Z_FSR_MU()
+      :MC_Z_FSR("MC_Z_FSR_MU")
+    {
+      _mode = 2;
+    }
+  };
+
   // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(MC_Z_FSR);
+  DECLARE_RIVET_PLUGIN(MC_Z_FSR_E);
+  DECLARE_RIVET_PLUGIN(MC_Z_FSR_MU);
+
+
+
 
 
 }
--- MC_Z_FSR_E.plot
+++ MC_Z_FSR_E.plot
@@ -1,58 +1,58 @@
-# BEGIN PLOT /MC_Z_FSR/photon_et_*
+# BEGIN PLOT /MC_Z_FSR_E/photon_et_*
 XLabel=$E_T$ [GeV]
 YLabel=$\mathrm{d}\sigma / \mathrm{d}E_T$ [pb/GeV]
 LogX=0
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/photon_et_all
+# BEGIN PLOT /MC_Z_FSR_E/photon_et_all
 Title=$Z \leftarrow \mu\mu\gamma$, Photon $E_T$
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/photon_et_wide
+# BEGIN PLOT /MC_Z_FSR_E/photon_et_wide
 Title=$Z \leftarrow \mu\mu\gamma$, Photon $E_T$  ($0.5 < \Delta R < 3.0$)
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/photon_et_close
+# BEGIN PLOT /MC_Z_FSR_E/photon_et_close
 Title=$Z \leftarrow \mu\mu\gamma$, Photon $E_T$ ($0.05 < \Delta R < 0.5$)
 # END PLOT
 
 
-# BEGIN PLOT /MC_Z_FSR/dr_*
+# BEGIN PLOT /MC_Z_FSR_E/dr_*
 XLabel=$\Delta R_{\gamma\mu}$
 YLabel=$\mathrm{d}\sigma / \mathrm{d}\Delta R_{\gamma\mu}$ [pb]
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/dr_all
+# BEGIN PLOT /MC_Z_FSR_E/dr_all
 Title=$Z \leftarrow \mu\mu\gamma$, $\Delta R_{\gamma\mu}$
 # END PLOT
 
 
-# BEGIN PLOT /MC_Z_FSR/dphi_all
+# BEGIN PLOT /MC_Z_FSR_E/dphi_all
 Title=$\Delta \Phi (\gamma,$ closest lepton)
 XLabel=$\Delta \Phi_{\gamma\mu}$
 YLabel=$\mathrm{d}\sigma / \mathrm{d}\Delta \Phi_{\gamma\mu}$ [pb]
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/deta_all
+# BEGIN PLOT /MC_Z_FSR_E/deta_all
 Title=$\Delta \eta (\gamma,$ closest lepton)
 XLabel=$\Delta \eta_{\gamma\mu}$
 YLabel=$\mathrm{d}\sigma / \mathrm{d}\Delta \eta_{\gamma\mu}$ [pb]
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/mZgamma
+# BEGIN PLOT /MC_Z_FSR_E/mZgamma
 Title = Invariant mass of $Z\gamma$
 XLabel= invariant mass($Z\gamma$)
 YLabel=$\text{d}\sigma_{Z\gamma} / \text{d} m^{Z\gamma}$ [pb/GeV]
 # END PLOT
 
-# BEGIN PLOT /MC_Z_FSR/m_gamma_lepton_next
+# BEGIN PLOT /MC_Z_FSR_E/m_gamma_lepton_next
 Title = Invariant mass ($\gamma$, closest lepton)
 XLabel= invariant mass(lepton, $\gamma$)
 YLabel=$\text{d}\sigma_{Z\gamma} / \text{d} m^{\mathrm{lepton} \gamma}$ [pb/GeV]
 # END PLOT
 
 
-# BEGIN PLOT /MC_Z_FSR/qT
+# BEGIN PLOT /MC_Z_FSR_E/qT
 Title = transverse momentum of both leptons and $\gamma$
 XLabel= ($q_\mathrm{T}$)
 YLabel=$\text{d}\sigma_{Z\gamma} / \text{d} q_\mathrm{T}$ [pb/GeV]

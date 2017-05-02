bdbeef4e6565db88cf3824a8471a6b0ccda25594
Johannes Krause <johannes.krause1@tu-dresden.de>
2017-05-02 17:37:24 +0200

    use now leading photon and require isolation to all particles which are not the selected leptons and itself

--- MC_Z_FSR.cc
+++ MC_Z_FSR.cc
@@ -4,14 +4,17 @@
 #include "Rivet/Projections/ChargedLeptons.hh"
 #include "Rivet/Projections/NeutralFinalState.hh"
 #include "Rivet/Projections/IdentifiedFinalState.hh"
+#include "Rivet/Projections/LeadingParticlesFinalState.hh"
 
 namespace Rivet {
 
    /*Cuts:  
     * 2 leptons, opposite charge, pT> 15 GeV, |eta| < 2.5 
     * photonen: pT>5GeV , |eta| < 2.5
-    *      -> highest pT-photon selected that does not come from a decay 
-    *         and has a dR in (0.05, 5) to the next myon
+    *      -> leading photon selected:
+    *           not from a decay 
+    *           dR in (0.05, 5) to the next lepton
+    *           isolated from everything else
     * lepton pair: invariant mass in (30, 87)
     * 
     *  */
@@ -34,11 +37,13 @@ namespace Rivet {
 
     /// Book histograms and initialise projections before the run
     void init() {
+      //all photons
       Cut c_photons = Cuts::pT >= 5.0*GeV && Cuts::abseta < 2.5;
-      IdentifiedFinalState photons(c_photons);
-      photons.acceptId(PID::PHOTON);
+      LeadingParticlesFinalState photons(c_photons);
+      photons.addParticleId(PID::PHOTON);
       declare(photons, "PHOTFS");
-
+      
+      //all leptons
       Cut c_leptons   = Cuts::pT > 15*GeV && Cuts::abseta < 2.5;
       IdentifiedFinalState leptonfs(c_leptons);
       if (_mode==1)  {
@@ -47,7 +52,11 @@ namespace Rivet {
       else leptonfs.acceptIdPair(PID::MUON);
       declare(leptonfs, "LEPFS");
 
+      //all particles
+      FinalState fs;
+      declare(fs, "FS");  
 
+      // book histograms      
       _hist_pho_et           = bookHisto1D("photon_et_all", 100, 5, 250);  // photon transverse energy
       _hist_pho_et_wide      = bookHisto1D("photon_et_wide", 100, 5, 250);  // photon transverse energy (0.5 < dr < 3.0)
       _hist_pho_et_close     = bookHisto1D("photon_et_close", 100, 5, 250);  // photon transverse energy (0.05 < dr < 0.5)
@@ -66,41 +75,44 @@ namespace Rivet {
     /// Perform the per-event analysis
     void analyze(const Event& event) {
 
-    
+      // lepton selection      
       const Particles leptonen = apply<IdentifiedFinalState>(event, "LEPFS").particlesByPt();
-
       if (leptonen.size() < 2) vetoEvent;
       if (leptonen[0].charge()*leptonen[1].charge() > 0) vetoEvent;
       const double mZ = ( leptonen[0].momentum() + leptonen[1].momentum()).mass();
       if (!inRange(mZ, 30*GeV, 87*GeV)) vetoEvent;
 
-      const Particles photons = apply<IdentifiedFinalState>(event, "PHOTFS").particlesByPt();
-      // We want the photon with the highest pT that does not come from a decay
-      foreach(const Particle& p, photons) {
-        if (p.fromDecay() || !p.isStable()) continue;
-        const double dR = std::min(deltaR(p,leptonen[0]), deltaR(p,leptonen[1]) );
-        if (!inRange(dR, 0.05, 5.0)) continue;
-
-        const double dphi = std::min(deltaPhi(p,leptonen[0]), deltaPhi(p,leptonen[1]) );
-        const double deta = std::min(deltaEta(p,leptonen[0]), deltaEta(p,leptonen[1]) );
-        const double qT = (leptonen[0].mom() + leptonen[1].mom() + p.mom()).pT();
-        const double mZgamma = (leptonen[0].mom() + leptonen[1].mom() + p.mom()).mass() * GeV; 
-
-				const Particle next_lepton = (deltaR(leptonen[0], p) > deltaR(leptonen[1], p) ? leptonen[0] : leptonen[1]);
-
-   			 // Fill the analysis histograms
-				_hist_pho_et->fill(p.pT()/GeV, event.weight());
-				_hist_pho_dr->fill(dR, event.weight());
-				_hist_pho_dphi->fill(dphi, event.weight());
-				_hist_pho_deta->fill(deta, event.weight());
-				_hist_mZgamma->fill(mZgamma, event.weight());
-				_hist_m_gl_next->fill((next_lepton.mom() + p.mom()).mass()*GeV, event.weight());
-				_hist_qt->fill(qT*GeV, event.weight());
-
-        (dR <= 0.5 ? _hist_pho_et_close : _hist_pho_et_wide)->fill(p.pT()/GeV, event.weight());
-
-        break; // Exit the loop since we found the highest pT lepton already
+      const Particles photons = apply<LeadingParticlesFinalState>(event, "PHOTFS").particles();
+      if (photons.empty()) vetoEvent;
+      const Particle & photon = photons[0];
+      Particles fs = apply<FinalState>(event, "FS").particles();
+      // check if the leading photon passes all cuts
+      if (photon.fromDecay() ) vetoEvent;
+      const double dR = std::min(deltaR(photon,leptonen[0]), deltaR(photon,leptonen[1]) );
+      if (!inRange(dR, 0.05, 5.0)) vetoEvent;
+      // check photon isolation
+      double coneEnergy(0.0);
+      foreach(const Particle& p, fs) {
+            if ( deltaR(photon, p) < 0.4 )  coneEnergy += p.E();
       }
+      if ( (coneEnergy - photon.E() - leptonen[0].E() - leptonen[1].E() ) >= 0.5*photon.E() )  vetoEvent;
+
+      const double dphi = std::min(deltaPhi(photon,leptonen[0]), deltaPhi(photon,leptonen[1]) );
+      const double deta = std::min(deltaEta(photon,leptonen[0]), deltaEta(photon,leptonen[1]) );
+      const double qT = (leptonen[0].mom() + leptonen[1].mom() + photon.mom()).pT();
+      const double mZgamma = (leptonen[0].mom() + leptonen[1].mom() + photon.mom()).mass() * GeV; 
+			const Particle next_lepton = (deltaR(leptonen[0], photon) > deltaR(leptonen[1], photon) ? leptonen[0] : leptonen[1]);
+   	  // Fill the analysis histograms
+			_hist_pho_et->fill(photon.pT()/GeV, event.weight());
+			_hist_pho_dr->fill(dR, event.weight());
+			_hist_pho_dphi->fill(dphi, event.weight());
+			_hist_pho_deta->fill(deta, event.weight());
+			_hist_mZgamma->fill(mZgamma, event.weight());
+			_hist_m_gl_next->fill((next_lepton.mom() + photon.mom()).mass()*GeV, event.weight());
+			_hist_qt->fill(qT*GeV, event.weight());
+
+      (dR <= 0.5 ? _hist_pho_et_close : _hist_pho_et_wide)->fill(photon.pT()/GeV, event.weight());
+
 
     }
 
